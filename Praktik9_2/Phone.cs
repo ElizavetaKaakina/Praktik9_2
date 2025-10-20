986e2229b23fd3bd57511648e713e559d569b2e3
User <User@LAPTOP-795BBJR4>
2025-10-21 00:41:45 +0300

    Добавьте файлы проекта.

---
+++ Praktik9_2/Phone.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik9_2
+{
+    internal class Phone
+    {
+        private string brend;
+        private int battarylevel;
+
+        public string Brend
+        {
+            get { return brend; }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    brend = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: строка пустая или null.");
+                }
+            }
+        }
+        public int BattaryLevel
+        {
+            get { return battarylevel; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    battarylevel = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: заряд батареи не может быть меньше %0 или больше %100.");
+                }
+            }
+        }
+
+        public void UsePhone(string brend, ref int battarylevel)
+        {
+            if (battarylevel > 0)
+            {
+                battarylevel -= 10;
+                Console.WriteLine($"Телефон {brend}, заряд %{battarylevel}.");
+            }
+            else
+            {
+                Console.WriteLine($"Телефон {brend}, разряжен.");
+            }
+        }
+    }
+}
---
+++ Praktik9_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik9_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone = new Phone();
+            phone.Brend = "Honor";
+            phone.BattaryLevel = 28;
+            int battary = phone.BattaryLevel;
+
+            phone.UsePhone(phone.Brend, ref battary);
+            phone.UsePhone(phone.Brend, ref battary);
+        }
+    }
+}
